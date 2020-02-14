2e2d9bde2d556e750ca733d3c88049bbc1f515a6
Daniel Holmberg (Dev) <danielholmberg.dev@gmail.com>
2020-02-14 08:29:07 +0100

    Improved Tribe tile design.

--- lib/screens/home/tabs/tribes/screens/JoinTribe.dart
+++ lib/screens/home/tabs/tribes/screens/JoinTribe.dart
@@ -2,6 +2,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tribes/models/Post.dart';
 import 'package:tribes/models/Tribe.dart';
 import 'package:tribes/models/User.dart';
 import 'package:tribes/services/database.dart';
@@ -309,6 +310,100 @@ class _JoinTribeState extends State<JoinTribe> {
       );
     }
 
+    _tribeTile(Tribe tribe) {
+      return GestureDetector(
+        onTap: () => _showPasswordDialog(tribe),
+        child: Container(
+          margin: EdgeInsets.all(6.0),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+              color: tribe.color,
+              borderRadius: BorderRadius.circular(20.0),
+              boxShadow: [
+                BoxShadow(
+                  color: tribe.color,
+                  blurRadius: 10,
+                  offset: Offset(0, 0),
+                ),
+              ]),
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              AutoSizeText(
+                tribe.name,
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                minFontSize: 10.0,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'TribesRounded',
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: StreamBuilder<List<Post>>(
+                  stream: DatabaseService().posts(tribe.id)
+                    .map((list) => list.documents
+                    .map((doc) => Post.fromSnapshot(doc))
+                    .toList()
+                  ),
+                  builder: (context, snapshot) {
+                    var postsList = snapshot.hasData ? snapshot.data : []; 
+
+                    return Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.view_list,
+                          color: Constants.buttonIconColor,
+                          size: 20,
+                        ),
+                        SizedBox(width: Constants.tinySpacing),
+                        Text(
+                          '${postsList.length}',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'TribesRounded',
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '${tribe.members.length}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'TribesRounded',
+                      ),
+                    ),
+                    SizedBox(width: Constants.tinySpacing),
+                    Icon(
+                      Icons.group,
+                      color: Constants.buttonIconColor,
+                      size: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return loading ? Loading() : Scaffold(
       body: SafeArea(
         child: Container(
@@ -324,74 +419,27 @@ class _JoinTribeState extends State<JoinTribe> {
                       _tribesList = snapshot.data;
 
                       return Container(
-                        padding: EdgeInsets.symmetric(horizontal: 12.0),
                         child: ScrollConfiguration(
                           behavior: CustomScrollBehavior(), 
                           child: _searchResult.length != 0 || controller.text.isNotEmpty
                           ? GridView.builder(
-                            padding: EdgeInsets.only(top: 80, bottom: 12.0),
+                            padding: EdgeInsets.fromLTRB(12.0, 80.0, 12.0, 12.0),
                             itemCount: _searchResult.length,
                             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: 2
                             ),
                             itemBuilder: (context, i) {
-                              Tribe tribe = _searchResult[i];
-                              return GestureDetector(
-                                onTap: () => _showPasswordDialog(tribe),
-                                child: Card(
-                                  color: tribe.color,
-                                  child: Container(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Center(
-                                      child: AutoSizeText(
-                                        tribe.name,
-                                        textAlign: TextAlign.center,
-                                        maxLines: 2,
-                                        minFontSize: 10.0,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'TribesRounded',
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
+                              return _tribeTile(_searchResult[i]); 
+                            }
                           )
                           : GridView.builder(
-                            padding: EdgeInsets.only(top: 80, bottom: 12.0),
+                            padding: EdgeInsets.fromLTRB(12.0, 80.0, 12.0, 12.0),
                             itemCount: _tribesList.length,
                             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: 2,
                             ),
                             itemBuilder: (context, i) {
-                              Tribe tribe = _tribesList[i];
-                              return GestureDetector(
-                                onTap: () => _showPasswordDialog(tribe),
-                                child: Card(
-                                  color: tribe.color,
-                                  child: Container(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Center(
-                                      child: AutoSizeText(
-                                        tribe.name,
-                                        textAlign: TextAlign.center,
-                                        maxLines: 2,
-                                        minFontSize: 10.0,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'TribesRounded',
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
+                              return _tribeTile(_tribesList[i]);
                             },
                           ),
                         ),
